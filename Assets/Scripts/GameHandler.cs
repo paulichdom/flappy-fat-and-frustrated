13e396fc8648d9d52b4cb58c5a93bffdbf206bdd
Domagoj <paulic.domagoj@gmail.com>
2024-11-02 18:50:52 +0100

    init 2d project, add basic setup

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+    private void Start()
+    {
+        Debug.Log("GameHandler.Start");
+    }
+}
