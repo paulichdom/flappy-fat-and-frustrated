069790f7a69a158e8d1d96639e79ac818be36552
Domagoj <paulic.domagoj@gmail.com>
2024-11-02 20:30:37 +0100

    add game assets, render sprite, modify script execution order

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+  private static GameAssets _instance;
+
+  public static GameAssets GetInstance()
+  {
+    return _instance;
+  }
+
+  private void Awake()
+  {
+    _instance = this;
+  }
+  
+  public Sprite pipeHeadSprite;
+}
--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -5,13 +5,9 @@ using CodeMonkey.Utils;
 public class GameHandler : MonoBehaviour {
     private void Start()
     {
-        Debug.Log("GameHandler.Start");
+      Debug.Log("GameHandler.Start");
 
-        int count = 0;
-        FunctionPeriodic.Create(() =>
-        {
-            CMDebug.TextPopupMouse("Ding! " + count);
-            count++;
-        }, .300f);
+       GameObject gameObject = new GameObject("Pipe", typeof(SpriteRenderer));
+       gameObject.GetComponent<SpriteRenderer>().sprite = GameAssets.GetInstance().pipeHeadSprite;
     }
 }
