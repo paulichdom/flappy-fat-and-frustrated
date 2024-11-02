e089bc3a1e7106923b3e131bf7a9b35bc68c75e5
Domagoj <paulic.domagoj@gmail.com>
2024-11-02 20:03:38 +0100

    test utils package

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
 
 public class GameHandler : MonoBehaviour {
     private void Start()
     {
         Debug.Log("GameHandler.Start");
+
+        int count = 0;
+        FunctionPeriodic.Create(() =>
+        {
+            CMDebug.TextPopupMouse("Ding! " + count);
+            count++;
+        }, .300f);
     }
 }
