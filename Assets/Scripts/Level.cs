ca834012073b080dcb17d316394121bf7a6dcec2
Domagoj <paulic.domagoj@gmail.com>
2024-11-09 17:04:33 +0100

    add top and bottom pipes

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -7,20 +7,38 @@ public class Level : MonoBehaviour {
     private const float PipeHeadHeight = 3.75f;
     
     public void Start() {
-        CreatePipe(50f,0f);
-        CreatePipe(40f,20f);
-        CreatePipe(30f,40f);
-        CreatePipe(20f,60f);
+        CreatePipe(20f,20f, true);
+        CreatePipe(40f,20f, false);
+
     }
     
-    private void CreatePipe(float height, float xPosition) {
+    private void CreatePipe(float height, float xPosition, bool createBottom) {
         // Set up Pipe Head
         Transform pipeHead = Instantiate(GameAssets.GetInstance().pfPipeHead);
-        pipeHead.position = new Vector3(xPosition, -CameraOrthoSize + height - PipeHeadHeight * 0.5f);
+        float pipeHeadYPosition;
+        
+        if (createBottom) {
+            pipeHeadYPosition = -CameraOrthoSize + height - PipeHeadHeight * 0.5f;
+        }
+        else {
+            pipeHeadYPosition = +CameraOrthoSize - height + PipeHeadHeight * 0.5f;
+        }
+        
+        pipeHead.position = new Vector3(xPosition, pipeHeadYPosition);
         
         // Set up Pipe Body
         Transform pipeBody = Instantiate(GameAssets.GetInstance().pfPipeBody);
-        pipeBody.position = new Vector3(xPosition, -CameraOrthoSize);
+        float pipeBodyYPosition;
+        
+        if (createBottom) {
+            pipeBodyYPosition = -CameraOrthoSize;
+        }
+        else {
+            pipeBodyYPosition = +CameraOrthoSize;
+            pipeBody.localScale = new Vector3(1, -1, 1);
+        }
+        
+        pipeBody.position = new Vector3(xPosition, pipeBodyYPosition);
         
         SpriteRenderer pipeBodySpriteRenderer = pipeBody.GetComponent<SpriteRenderer>();
         pipeBodySpriteRenderer.size = new Vector2(PipeWidth, height);
